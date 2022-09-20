94b011e589041f335e0a5b5e11bd5469623f8034
Karen267248 <111141123+Karen267248@users.noreply.github.com>
2022-09-20 16:59:34 -0500

    trabajo de entradas y salidas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    private final static double PI = 3.14;
+    private static int numero1;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in) ;
+        System.out.println("Digite su primer numero");
+        numero1 = input.nextInt();
+        System.out.printIn("resultado: " + (numero1 * PI));
     }
 }
